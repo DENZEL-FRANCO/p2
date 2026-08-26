5db2d5efd0db72c93df258b658ac104cb07cbbab
DENZEL-FRANCO <denzelfranco786@gmail.com>
2026-08-25 21:40:49 -0300

    aula3

---
+++ aula3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class aula3 {
+
+public static void main(String[] orgs){
+
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.print("digite sua primeira nota");
+    double primeira = entrada.nextDouble();
+
+    System.out.print("digite sua seguda nota");
+    double segunda = entrada.nextDouble();
+
+    System.out.print("digite sua terceira nota");
+    double terceira = entrada.nextDouble();
+    double media = ((primeira + segunda + terceira) / 3);8
+
+    System.out.println("=====RESULTADO=====");
+
+    boolean aprovado = media >= 7;
+
+    System.out.println("média; " + media);
+    System.out.println("aprovado: " + aprovado);
+
+    entrada.close();
+
+    }   
+}
