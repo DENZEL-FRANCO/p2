c4dcd054b9ada034dd11098a9a4b3be86eda21a0
DENZEL-FRANCO <denzelfranco786@gmail.com>
2026-08-25 21:42:03 -0300

    aula3

---
+++ aula3_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class aula3_2 {
+
+  public static void main(String[] orgs){
+  
+  Scanner entrada = new Scanner(System.in);
+
+  System.out.print("preço: ");
+  double preco = entrada.nextDouble();
+
+  System.out.print("produto");
+  double produto = entrada.nextDouble();
+
+  System.out.print("desconto");
+  double
+  }
+}
