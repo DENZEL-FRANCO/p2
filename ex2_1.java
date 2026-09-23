3855ed7c9577b6c730718f94d417f8c961cd1a46
DENZEL-FRANCO <denzelfranco786@gmail.com>
2026-09-22 21:17:26 -0300

    continua

---
+++ ex2_1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import  java.util.Scanner;
+
+public class ex2_1 {
+    public static void main(String [] orgs){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("digite o primeiro número: ");
+        int primeiro = entrada.nextInt();
+
+        System.out.print("digite o segundo número: ");
+        int segundo = entrada.nextInt();
+
+        System.out.print("digite o terceiro número: ");
+        int terceiro = entrada.nextInt();
+
+        System.out.print("digite o quarto número: ");
+        int quarto = entrada.nextInt();
+
+        System.out.print("digite o quinto número: ");
+        int quinto = entrada.nextInt();
+
+       //lista vazia
+       ArrayList<Integer> vazia = new ArrayList<>();
+
+    }
+    
+}
---
+++ ex2_2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex2_2 {
+    public static void main(String[]orgs) {
+        Scanner entrada = new Scanner(System.in);
+
+         //lista vazia
+       ArrayList<Integer> precos = new ArrayList<>();
+       
+       System.out.print("digite o primeiro preço");
+       int p1 = entrada.nextInt();
+       precos.add("p1")
+
+    }
+    
+}
